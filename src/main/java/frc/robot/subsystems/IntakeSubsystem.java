feed6e5a2a713d55230a442636562d3590b1fff9
Quintavius <mazorimri2@gmail.com>
2025-01-19 21:19:55 +0200

    made intake
    uses spark flex

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Subsystems.Intake;
+public class IntakeSubsystem extends SubsystemBase {
+    // add constants
+
+    private SparkFlex sparkFlex;
+
+    private RelativeEncoder encoder;
+
+    public IntakeSubsystem() {
+        sparkFlex = new SparkFlex(Intake.Motors.SPARK_FLEX_ID, MotorType.kBrushless);// add motor id
+        this.encoder = sparkFlex.getEncoder();
+    }
+
+    public void moveMotor(double percentage) {
+        this.sparkFlex.set(percentage);
+    }
+
+    public void stopMotor() {
+        this.sparkFlex.set(0);
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("current percentage", sparkFlex.get());
+        SmartDashboard.putNumber("current velcoity RPM", encoder.getVelocity());
+    }
+
+}
