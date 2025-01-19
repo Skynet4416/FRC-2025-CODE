d17552d1f045df06bceaf20c46c8521ed044c070
Quintavius <mazorimri2@gmail.com>
2025-01-19 21:24:29 +0200

    made intake go out

---
+++ src/main/java/frc/robot/commands/IntakeOutwardCommands.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import frc.robot.Constants.Subsystems.Intake;
+import frc.robot.subsystems.IntakeSubsystem;
+
+import com.revrobotics.spark.SparkFlex;
+
+import edu.wpi.first.wpilibj2.command.Command;
+public class IntakeOutwardCommands extends Command{
+    private final IntakeSubsystem intakeSubsystem;
+
+    public IntakeOutwardCommands(IntakeSubsystem intakeSubsystem){
+        this.intakeSubsystem = intakeSubsystem;
+        addRequirements(intakeSubsystem);
+    }
+
+    @Override
+    public void initialize(){
+        intakeSubsystem.moveMotor(-Intake.Physical.INTAKE_PERCENTAGE);
+    }
+    @Override
+    public void execute(){
+    }
+    @Override
+    public void end(boolean inturrupted){
+        intakeSubsystem.stopMotor();
+    }
+    
+    @Override
+    public boolean isFinished() {
+       return false;
+    }
+}
