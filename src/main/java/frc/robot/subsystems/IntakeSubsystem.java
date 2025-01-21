65857d51175bb94e8a43e4dfb162596f57d64556
Quintavius <mazorimri2@gmail.com>
2025-01-21 19:54:29 +0200

    changed to meaningful names

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -10,27 +10,27 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Subsystems.Intake;
 public class IntakeSubsystem extends SubsystemBase {
 
-    private SparkFlex sparkFlex;
+    private SparkFlex intakeSparkFlex;
 
-    private RelativeEncoder encoder;
+    private RelativeEncoder intakeFlexEncoder;
 
     public IntakeSubsystem() {
-        sparkFlex = new SparkFlex(Intake.Motors.SPARK_FLEX_ID, MotorType.kBrushless);
-        this.encoder = sparkFlex.getEncoder();
+        intakeSparkFlex = new SparkFlex(Intake.Motors.SPARK_FLEX_ID, MotorType.kBrushless);
+        this.intakeFlexEncoder = intakeSparkFlex.getEncoder();
     }
 
     public void moveMotor(double percentage) {
-        this.sparkFlex.set(percentage);
+        this.intakeSparkFlex.set(percentage);
     }
 
     public void stopMotor() {
-        this.sparkFlex.set(0);
+        this.intakeSparkFlex.set(0);
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("current percentage", sparkFlex.get());
-        SmartDashboard.putNumber("current velcoity RPM", encoder.getVelocity());
+        SmartDashboard.putNumber("current percentage", intakeSparkFlex.get());
+        SmartDashboard.putNumber("current velcoity RPM", intakeFlexEncoder.getVelocity());
     }
 
 }
