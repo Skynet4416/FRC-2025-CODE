6aa1e443ca13d82d7aa82cbe8d03d7aeb26e8e95
mrreguy2 <mazorimri2@gmail.com>
2025-01-23 21:06:07 +0200

    updated itake to 2 flex motors
    made lower motor slave

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
@@ -8,29 +9,39 @@ import com.revrobotics.spark.config.SparkFlexConfig;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Subsystems.Intake;
+
 public class IntakeSubsystem extends SubsystemBase {
 
-    private SparkFlex intakeSparkFlex;
+    private final SparkFlex upperIntakeSparkFlex;
+    private final SparkFlex lowerIntakeSparkFlex;
 
-    private RelativeEncoder intakeFlexEncoder;
+    private final RelativeEncoder upperMasterIntakeFlexEncoder;
 
     public IntakeSubsystem() {
-        intakeSparkFlex = new SparkFlex(Intake.Motors.SPARK_FLEX_ID, MotorType.kBrushless);
-        this.intakeFlexEncoder = intakeSparkFlex.getEncoder();
+        upperIntakeSparkFlex = new SparkFlex(Intake.Motors.UPPER_MASTER_SPARK_FLEX_ID, MotorType.kBrushless);
+        lowerIntakeSparkFlex = new SparkFlex(Intake.Motors.LOWER_SLAVE_SPARK_FLEX_ID, MotorType.kBrushless);
+        
+        upperIntakeSparkFlex.configure(new SparkFlexConfig(), SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+        SparkFlexConfig slaveConfig = new SparkFlexConfig();
+        slaveConfig.follow(upperIntakeSparkFlex,true);
+
+        lowerIntakeSparkFlex.configure(slaveConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+        this.upperMasterIntakeFlexEncoder = upperIntakeSparkFlex.getEncoder();
     }
 
     public void moveMotor(double percentage) {
-        this.intakeSparkFlex.set(percentage);
+        this.upperIntakeSparkFlex.set(percentage);
     }
 
     public void stopMotor() {
-        this.intakeSparkFlex.set(0);
+        this.upperIntakeSparkFlex.set(0);
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("current percentage", intakeSparkFlex.get());
-        SmartDashboard.putNumber("current velcoity RPM", intakeFlexEncoder.getVelocity());
+        SmartDashboard.putNumber("current percentage", upperIntakeSparkFlex.get());
+        SmartDashboard.putNumber("current velocity RPM", upperMasterIntakeFlexEncoder.getVelocity());
     }
 
 }
