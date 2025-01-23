57a85d4f098aea6cce6374f4e393a6a5e5c18f22
bobthebuilder98765 <mazorimri2@gmail.com>
2025-01-23 20:12:21 +0200

    made subsystem for deep cage climb(it think)

---
+++ src/main/java/frc/robot/subsystems/ClimbDeepSubsystem.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Subsystems.DeepCage;
+
+public class ClimbDeepSubsystem extends SubsystemBase {
+
+    private SparkMax DeepClimbSparkMax;
+    private DigitalInput ClimbLegLimitSwitch = new DigitalInput(0);
+
+
+    public ClimbDeepSubsystem() {
+        DeepClimbSparkMax = new SparkMax(DeepCage.Motors.SPARK_MAX_ID, MotorType.kBrushless);
+    }
+    
+    public void limitSwitchChecker(){
+        this.ClimbLegLimitSwitch.get();
+    }
+
+    public void moveMotor(double percentage) {
+        this.DeepClimbSparkMax.set(percentage); 
+    }
+
+    public void stopMotor() {
+        this.DeepClimbSparkMax.set(0);
+    }
+
+}
