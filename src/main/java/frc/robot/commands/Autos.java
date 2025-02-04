3d6bb68dfb44f9e362eba607f92761fdfa5360b9
yotam201410 <yotam201410@gmail.com>
2025-02-04 09:17:15 +0200

    added auto and telemetry

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,13 +1,24 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
-public final class Autos {
-  /** Example static factory for an autonomous command. */
+import choreo.auto.AutoFactory;
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
-  }
+public class Autos {
+    private final AutoFactory m_factory;
+
+    public Autos(AutoFactory factory) {
+        m_factory = factory;
+    }
+
+    public AutoRoutine getAutoRoutine(String routineName) {
+        final AutoRoutine routine = m_factory.newRoutine(routineName);
+        final AutoTrajectory simplePath = routine.trajectory(routineName);
+
+        routine.active().onTrue(
+                simplePath.resetOdometry()
+                        .andThen(simplePath.cmd())
+        );
+        return routine;
+    }
 }
