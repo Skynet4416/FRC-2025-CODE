4aee8afdd7c6dafd5016e471d4dc718714f7fd49
yotam <yotam201410@gmail.com>
2025-02-23 21:59:05 +0200

    auto from target

--- src/main/java/frc/robot/commands/AutoCommands/shaktonomousCommand.java
+++ src/main/java/frc/robot/commands/AutoCommands/shaktonomousCommand.java
@@ -1,4 +1,5 @@
 package frc.robot.commands.AutoCommands;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -16,11 +17,12 @@ public class shaktonomousCommand extends Command {
     private final PIDController yPID;
     private final PIDController rotationPID;
     private Pose2d robotpose;
-    private final Pose2d target = new Pose2d(5, 4, Rotation2d.fromDegrees(0));
+    private final Pose2d target;
     private final double offset = 0.2;
 
-    public shaktonomousCommand(CommandSwerveDrivetrain drive) {
+    public shaktonomousCommand(CommandSwerveDrivetrain drive, Pose2d target) {
         this.drive = drive;
+        this.target = target;
         xPID = new PIDController(1, 0, 0);
         yPID = new PIDController(1, 0, 0);
         rotationPID = new PIDController(0.8, 0, 0);
@@ -40,44 +42,45 @@ public class shaktonomousCommand extends Command {
         if (!tv) {
             return;
         }
-    
+
         robotpose = LimelightHelpers.getBotPose2d_wpiBlue("");
-        if (robotpose == null) return;
-    
+        if (robotpose == null)
+            return;
+
         // Debug output - add these to see what's happening
         SmartDashboard.putNumber("Target X", target.getX());
         SmartDashboard.putNumber("Target Y", target.getY());
         SmartDashboard.putNumber("Robot X", robotpose.getX());
         SmartDashboard.putNumber("Robot Y", robotpose.getY());
         SmartDashboard.putNumber("Robot Rotation", robotpose.getRotation().getDegrees());
-        
+
         // Calculate errors
         double xError = target.getX() - robotpose.getX();
         double yError = target.getY() - robotpose.getY();
-        
+
         SmartDashboard.putNumber("X Error", xError);
         SmartDashboard.putNumber("Y Error", yError);
-    
+
         double xCorrection = MathUtil.clamp(xPID.calculate(robotpose.getX(), target.getX()), -1, 1);
         double yCorrection = MathUtil.clamp(yPID.calculate(robotpose.getY(), target.getY()), -1, 1);
-        
+
         // Debug PID outputs
         SmartDashboard.putNumber("X Correction", xCorrection);
         SmartDashboard.putNumber("Y Correction", yCorrection);
-    
+
         ChassisSpeeds chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
-            xCorrection,
-            yCorrection,
-            0,  // Removed rotation for now to simplify
-            robotpose.getRotation()
-        );
-    
+                xCorrection,
+                yCorrection,
+                0, // Removed rotation for now to simplify
+                robotpose.getRotation());
+
         drive.setControl(drive.m_pathApplyRobotSpeeds.withSpeeds(chassisSpeeds));
     }
 
     @Override
     public boolean isFinished() {
-        if(robotpose == null) return false;
+        if (robotpose == null)
+            return false;
         double xError = Math.abs(robotpose.getX() - target.getX());
         double yError = Math.abs(robotpose.getY() - target.getY());
         return Math.abs(xError) < offset && Math.abs(yError) < offset;
